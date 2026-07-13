1472baf40479e5876353ddc1c3cbc26db5615e12
hkupiserang-gif <hkupiserang@gmail.com>
2026-07-13 22:18:58 +0700

    sw.js

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'horison-ot-v1';
+const urlsToCache = [
+  '/',
+  '/ot',
+  '/manifest.json',
+  '/icon-192.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
